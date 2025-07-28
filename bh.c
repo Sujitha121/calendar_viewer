2ea7a2c0cccd0ec74d0a14b395c7f70be5b44269
BhavanaBgowda13 <bhavana.b@s.amity.edu>
2025-07-28 12:10:58 +0530

    bh3

--- bh.c
+++ bh.c
@@ -4,4 +4,7 @@ int main() {
     int n, sum = 0;
     printf("Enter number of elements: ");
     scanf("%d", &n);
+int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
 
